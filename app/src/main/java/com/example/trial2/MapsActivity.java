b5be9386c273901aa3b1607be1bd51142f9b1828
skoumo01 <skoumo01@ucy.ac.cy>
2019-11-24 12:46:59 +0200

    first map push

---
+++ app/src/main/java/com/example/trial2/MapsActivity.java
@@ -0,0 +1,84 @@
+package com.example.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        this.mMap = map;
+
+        while(this.getIntent() == null) {
+            //wait
+        }
+
+        // Sets the map type to be "normal"
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //set the coordinates (cyprus coordinates)
+        LatLng UCY = new LatLng(35.144613,33.411215);
+        LatLng home = new LatLng(35.169746,33.319597);
+        LatLng driver = new LatLng(35.155579,33.312659);
+
+
+        //create a marker about the coordinates of latlng and add it to map
+        Marker ucyMarker = map.addMarker(new MarkerOptions()
+                .position(UCY)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        Marker homeMarker = map.addMarker(new MarkerOptions()
+                .position(home)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        Marker driverMarker = map.addMarker(new MarkerOptions()
+                .position(driver)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCY, 8.0f));
+
+    }
+
+}
